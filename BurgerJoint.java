02190b7282d8f172d280debc1ec53b2b7fea24e2
SavannahBolger <C00215747@itcarlow.ie>
2020-01-09 12:03:38 +0000

    base code

---
+++ BurgerJoint.java
@@ -0,0 +1,52 @@
+public class BurgerJoint {
+	static int buf;
+	static int p = 0;
+	static int c = 0;
+	static int n = 5;
+	public static class Producer extends Thread{
+		int a = 0;
+		public void run(){
+			System.out.println("Producer Starting");
+			while(p < n){
+				try{
+				Thread.sleep(1000);
+				}
+				catch(InterruptedException e){}
+				while(p != c){
+					System.out.println("Short order cook spinning");
+				}
+				a = (int)Math.random() * 100;
+				System.out.println("Making a Burger: " + p);
+				buf = a;
+				p = p + 1;
+			}
+			System.out.println("Finished Making Burgers");
+		}
+	}
+	public static class Consumer extends Thread{
+		int b = 0;
+		public void run(){
+			System.out.println("Consumer Starting");
+			while(c < n){
+				try{
+					Thread.sleep(2000);
+					}
+					catch(InterruptedException e){}
+				while(p <= c){
+					System.out.println("Starving waiting on a burger!");
+				}
+				System.out.println("Eating Burger: " + c);
+				b = buf;
+				c = c + 1;
+				
+			}
+			System.out.println("Finished Eating Burgers");
+		}
+	}
+	public static void main(final String[] args) {
+		final Producer producer = new Producer();
+		final Consumer consumer = new Consumer();
+		producer.start();
+		consumer.start();
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Y4-ProducerConsumer
+# Games Engineering II - Producer Consumer #
+
+Producer Consumer example, modify as per laboratory specification
+
+### What is this repository for? ###
+
+* Getting started with concurrent programming. Simple producer consumer example
+
+### How do I get set up? ###
+
+* Download and install [Java JDK](https://www.oracle.com/technetwork/java/javase/downloads/index.html)
+
+* Open Git Bash and clone the project `git clone https://bitbucket.org/MuddyGames/games-engineering-ii-producer-consumer.git`
+
+* Change to the project directory `cd games-engineering-ii-producer-consumer`
+
+* List files that were downloaded `ls`
+
+* From command line
+	* compile code using `javac BurgerJoint.java`
+	* execute code using `java BurgerJoint`
+
+### Who do I talk to? ###
+
+* philip.bourke@itcarlow.ie
